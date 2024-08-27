03e57fb8d5b34b1f8e6ebcf3802206c2b2a741e7
ElhoussinRazouki <razzoukihoussin@gmail.com>
2024-08-27 23:25:50 +0100

    chore: Remove Twilio integration and unused code
    ```

--- src/config/sms.config.ts
+++ src/config/sms.config.ts
@@ -1,5 +1,3 @@
-import twilio from 'twilio';
 import { environment } from '../utils/loadEnvironment';
 
 
-export const client = twilio(environment.TWILIO_ACCOUNT_SID, environment.TWILIO_AUTH_TOKEN);
