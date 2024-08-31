4e86569136e0c2b3a3355ffea77349a8f2b537e2
ElhoussinRazouki <razzoukihoussin@gmail.com>
2024-08-31 15:51:53 +0100

    chore: Remove unique index on userName in UserSchema

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -51,7 +51,6 @@ const UserSchema = new Schema({
 });
 
 UserSchema.index({ email: 1 }, { unique: true });
-UserSchema.index({ userName: 1 }, { unique: true });
 UserSchema.index({ refreshToken: 1 }, { unique: true });
 
 
