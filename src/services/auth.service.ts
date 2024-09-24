188f00f96f21e8b1684245f8fd5b8ac449339815
ElhoussinRazouki <razzoukihoussin@gmail.com>
2024-09-24 15:48:37 +0100

    fix token issue

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -82,7 +82,7 @@ export const verifyUserEmail = async (email: string, code: number) => {
         throw new Error('verification failed');
     }
     if (user.verificationCode === code) {
-        const accessToken = await generateTokenForUser({...user.toJSON(), isVerified: true});
+        const accessToken = await generateTokenForUser({...user.toJSON(), id: user.id , isVerified: true});
         const refreshToken = await generateRefreshTokenForUser(user.id);
         await Users.updateOne({ email }, { $set: { isVerified: true, accessToken, refreshToken, updated_at: new Date() } });
         return { accessToken, refreshToken };
