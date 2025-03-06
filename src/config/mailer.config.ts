bd26543a41691a82fda4b574aaa265d829acedd9
ElhoussinRazouki <razzoukihoussin@gmail.com>
2025-03-06 11:49:26 +0000

    use secure connection instead

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -4,14 +4,10 @@ import { environment } from '../utils/loadEnvironment';
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // Use STARTTLS instead of implicit SSL
+    port: 465,
+    secure: true, // Use STARTTLS instead of implicit SSL
     auth: {
         user: environment.EMAIL_USER,
         pass: environment.EMAIL_PASSWORD
     },
-    // Add timeout settings to avoid hanging connections
-    connectionTimeout: 10000, // 10 seconds
-    greetingTimeout: 5000,   // 5 seconds
-    socketTimeout: 10000     // 10 seconds
 });
